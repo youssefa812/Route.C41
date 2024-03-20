9f7cf1c13f11bee7bf9f78d96af20b6856cf0b6b
Melad <Melad@DESKTOP-7EUM97G>
2024-03-20 17:13:34 +0200

    Add project files.

---
+++ Route.C41.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Route.C41.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
